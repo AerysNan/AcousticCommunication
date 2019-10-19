86286169d7af2981184e947f27f2b4292ce00fc5
7czzl <zzl850783164@163.com>
2019-10-20 00:24:33 +0800

    bug phase

--- Main.m
+++ Main.m
@@ -11,9 +11,9 @@ sampling_span = 1 / sampling_frequency;
 signal_length = 5000;
 
 sampling_point = 0: sampling_span: (signal_length - 1) * sampling_span;
-preamble = [0 0 0 0 0 0 0 0];
+preamble = [0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1];
 data = [0 0 0 1 1 0 1 1 1 1 1 0 0 1 0 0];
-data = [preamble, data];
+data = [data, data, data, data];
 
 qpsk_length = 2; % qpsk encode per 2 bits
 ofdm_length = 8; % ofdm encode per 8 bits
@@ -37,15 +37,22 @@ signal_output_preamble = Carrier(signal_baseband_preamble, ...
     sampling_span, carrier_amplitude, carrier_frequency);
 
 
+
+
 figure(1);
 subplot(2, 1, 1);
 plot(carrier_point, signal_baseband, "LineWidth", 0.5);
 xlabel("Time");
 ylabel("Baseband Signal");
-grid on;
 
 %signal_output = signal_output / max(signal_output);
 signal_output = mapminmax(signal_output); %normalization to [-1.0, 1.0]
+signal_output_preamble = mapminmax(signal_output_preamble); %normalization to [-1.0, 1.0]
+signal_zeros = zeros(1, 10000);
+signal_preamble = [signal_zeros, signal_output_preamble, signal_zeros];
+
+signal_output = [signal_preamble, signal_output];
+
 subplot(2, 1, 2);
 plot(signal_output, "LineWidth", 0.5);
 xlabel("Time");
@@ -55,35 +62,52 @@ grid on;
 %signal_received = awgn(signal_output, 5);
 
 %% genSound
-fs = 48e3;
+fs = 44100;
 soundFile = 'genSound.wav';
 
 audiowrite(soundFile, signal_output, fs, 'BitsPerSample', 16);
 %sound(signal_output, fs);
 
 %% anaSound
+soundFile = 'receivedSound.wav';
+
 [signal_received, fs] = audioread(soundFile);
 signal_received = signal_received';
 
 %% insert blank of recording
-signal_blank = rand(1, 1024);
-signal_received = [signal_blank, signal_received];
-
-%% acquire the target signal's position
-[C21,lag21] = xcorr(signal_received, signal_output_preamble);
-C21 = C21/max(C21);
-
-[M21,I21] = max(C21);
-t21 = lag21(I21)+1;
-
-signal_received = signal_received(t21 : end);
+% signal_blank = rand(1, 10234)*2-1;
+% signal_received = [signal_blank, signal_received, signal_blank];
 
 figure(2);
-subplot(2, 1, 1);
+subplot(3, 1, 1);
 plot(signal_received, "LineWidth", 0.5);
 xlabel("Time");
 ylabel("Received Signal");
 grid on;
+
+%% acquire the target signal's position
+[C21,lag21] = xcorr(signal_received, signal_output_preamble);
+C21 = C21/max(C21);
+[~, I21] = max(C21);
+t21 = lag21(I21)-1000;
+
+signal_received = signal_received(t21 : end);
+
+%% second align
+[C21,lag21] = xcorr(signal_received, signal_output_preamble);
+C21 = C21/max(C21);
+[~, I21] = max(C21);
+t21 = lag21(I21)+20001;
+
+signal_received = signal_received(t21 : t21+length(carrier_wave)-1);
+
+
+subplot(3, 1, 2);
+plot(signal_received, "LineWidth", 0.5);
+xlabel("Time");
+ylabel("Received Signal");
+grid on;
+
 
 
 %% decode
@@ -110,7 +134,7 @@ for i = 1: signal_length: length(signal_adjusted)
     decode_data(pos: pos + ofdm_length - 1) = decode_clip;
 end
 
-subplot(2, 1, 2);
+subplot(3, 1, 3);
 plot(signal_filtered, "LineWidth", 0.5);
 xlabel("Time");
 ylabel("Filtered Signal");
