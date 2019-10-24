d64ed0c094a68a89acd4fa258c2a116cce0e9ed5
AerysNan <aerysnan@gmail.com>
2019-10-24 15:39:28 +0800

    add cyclic prefix

--- Demodulate.m
+++ Demodulate.m
@@ -6,29 +6,35 @@ simulate = input("Run in simulate mode: ");
 %% anaSound
 base_frequency = 500;
 signal_length = 882;
+header_length = 32;
+signal_real_length = signal_length + header_length;
 sampling_frequency = 44100;
-signal_time = signal_length / sampling_frequency;
+signal_time = signal_real_length / sampling_frequency;
 sampling_span = 1 / sampling_frequency;
-carrier_frequency = 10000;
 psk_length = 2; % qpsk encode per 2 bits
 ofdm_length = 8; % ofdm encode per 8 bits
+carrier_frequency = 10000;
 
-chirp_time = 0.1;
-chirp_frequency = 1000;
-chirp_x_axis = 0: sampling_span: chirp_time - sampling_span;
-signal_u_chirp = chirp(chirp_x_axis, 0, chirp_time * 2, chirp_frequency);
-signal_d_chirp = chirp(chirp_x_axis, 0, chirp_time, chirp_frequency / 2);
+chirp_u_time = 0.2;
+chirp_u_begin_frequency = 200;
+chirp_u_end_frequency = 600;
+chirp_d_time = 0.1;
+chirp_d_begin_frequency = 600;
+chirp_d_end_frequency = 1000;
+signal_u_chirp = chirp(0: sampling_span: chirp_u_time - sampling_span, chirp_u_begin_frequency, chirp_u_time, chirp_u_end_frequency);
+signal_d_chirp = chirp(0: sampling_span: chirp_d_time - sampling_span, chirp_d_begin_frequency, chirp_d_time, chirp_d_end_frequency);
 
 if simulate
     soundFile = 'output.wav';
     [signal_output, ~] = audioread(soundFile);
-%    signal_received = signal_output;
     signal_received = awgn(signal_output, 10);
+    signal_received = signal_received(:, 1);
     signal_received = signal_received';
     signal_received = signal_received(1 + length(signal_u_chirp): end - length(signal_d_chirp));
 else
     soundFile = 'received.wav';
     [signal_received, fs] = audioread(soundFile);
+    signal_received = signal_received(:, 1);
     signal_received = signal_received';
     [C, lag] = xcorr(signal_received, signal_u_chirp);
     [~, I] = max(C);
@@ -36,11 +42,11 @@ else
     signal_received = signal_received(begin: end);
     [C, lag] = xcorr(signal_received, signal_d_chirp);
     [~, I] = max(C);
-    finish = round(lag(I) / signal_length) * signal_length;
+    finish = round(lag(I) / signal_real_length) * signal_real_length;
     signal_received = signal_received(1: finish);
 end
 
-content_time = signal_time * length(signal_received) / signal_length;
+content_time = signal_time * length(signal_received) / signal_real_length;
 offset_frequency = 10;
 max_frequency = base_frequency * ofdm_length / psk_length;
 
@@ -50,13 +56,15 @@ ylabel("Received Signal");
 grid on;
 
 %% decode
-decode_data = zeros(1, ofdm_length * length(signal_received) / 2 / signal_length);
+decode_data = zeros(1, ofdm_length * length(signal_received) / 2 / signal_real_length);
 
-for i = 1: 2 * signal_length: length(signal_received)
-    clip = Carrier(signal_received(i: i + signal_length - 1), sampling_span, carrier_frequency);
-    clip_filtered = BPassFilter(clip, base_frequency - offset_frequency, max_frequency + offset_frequency, sampling_frequency);
+for i = 1: 2 * signal_real_length: length(signal_received)
+    clip = signal_received(i: i + signal_real_length - 1);
+    clip_carrier = Carrier(clip, sampling_span, carrier_frequency);
+    clip_carrier = clip_carrier(1 + header_length: end);
+    clip_filtered = BPassFilter(clip_carrier, base_frequency - offset_frequency, max_frequency + offset_frequency, sampling_frequency);
     decode_clip = OFDMDecode(clip_filtered, ofdm_length, psk_length);
-    pos = (i - 1) * ofdm_length / 2 / signal_length + 1;
+    pos = (i - 1) * ofdm_length / 2 / signal_real_length + 1;
     decode_data(pos: pos + ofdm_length - 1) = decode_clip;
 end
 
--- Modulate.m
+++ Modulate.m
@@ -5,39 +5,45 @@ clc;
 %% encode
 base_frequency = 500;
 signal_length = 882;
+header_length = 32;
+signal_real_length = signal_length + header_length;
 sampling_frequency = 44100;
-signal_time = signal_length / sampling_frequency;
+signal_time = signal_real_length / sampling_frequency;
 sampling_span = 1 / sampling_frequency;
 psk_length = 2; % qpsk encode per 2 bits
 ofdm_length = 8; % ofdm encode per 8 bits
 carrier_frequency = 10000;
 
 data = [0 0 0 1 1 0 1 1];
-data = repmat(data, 1, 5);
+data = repmat(data, 1, 20);
 disp(data);
-signal_output = zeros(1, 2 * signal_length * length(data) / ofdm_length);
-signal_zero = zeros(1, signal_length);
+signal_output = zeros(1, 2 * signal_real_length * length(data) / ofdm_length);
+signal_zero = zeros(1, signal_real_length);
 for i = 1: ofdm_length: length(data)
     signal_clip = OFDMEncode(data(i: i + ofdm_length - 1), base_frequency, psk_length, ofdm_length, sampling_frequency, signal_length);
+    signal_clip = [signal_clip(end - header_length + 1: end) signal_clip];
     signal_clip = Carrier(signal_clip, sampling_span, carrier_frequency);
     pos = (i - 1) / ofdm_length;
-    signal_output(2 * pos * signal_length + 1: 2 * (pos + 1) * signal_length) = [signal_clip signal_zero];
+    signal_output(2 * pos * signal_real_length + 1: 2 * (pos + 1) * signal_real_length) = [signal_clip signal_zero];
 end
 signal_output = signal_output / max(abs(signal_output));
 
 content_time = length(data) / ofdm_length * signal_time * 2;
 
-chirp_time = 0.1;
-chirp_frequency = 1000;
-chirp_x_axis = 0: sampling_span: chirp_time - sampling_span;
-signal_u_chirp = chirp(chirp_x_axis, 0, chirp_time * 2, chirp_frequency);
-signal_d_chirp = chirp(chirp_x_axis, 0, chirp_time, chirp_frequency / 2);
+chirp_u_time = 0.2;
+chirp_u_begin_frequency = 200;
+chirp_u_end_frequency = 600;
+chirp_d_time = 0.1;
+chirp_d_begin_frequency = 600;
+chirp_d_end_frequency = 1000;
+signal_u_chirp = chirp(0: sampling_span: chirp_u_time - sampling_span, chirp_u_begin_frequency, chirp_u_time, chirp_u_end_frequency);
+signal_d_chirp = chirp(0: sampling_span: chirp_d_time - sampling_span, chirp_d_begin_frequency, chirp_d_time, chirp_d_end_frequency);
 
 zero_time = 0.1;
 signal_zero = zeros(1, zero_time * sampling_frequency);
 signal_output = [signal_u_chirp signal_output signal_d_chirp];
 
-total_time = content_time + 2 * chirp_time;
+total_time = content_time + chirp_u_time + chirp_d_time;
 plot(0: sampling_span: total_time - sampling_span, signal_output, "LineWidth", 0.5);
 xlabel("Time");
 ylabel("Output Signal");
